c018595d76fe1ceaecaaefe0e97a54c197677d8d
Stephanie Livengood <sreneelive@gmail.com>
2024-07-20 14:00:52 -0400

    fix routing

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use((req, res, next)=> {
 app.use('/', workoutRoutes)
 app.use('/api/workouts', workoutRoutes);
 
-
+app.get('*',function (req, res) {
+    res.redirect('/');
+});
 //connect to db
 mongoose.connect(process.env.MONGO_URI).then(() => {
     //move app listen here. listen only once db is connected
