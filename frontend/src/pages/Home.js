2d84ed07c6c80da6a54dbe224ca3305a1f6d1ce3
Stephanie Livengood <sreneelive@gmail.com>
2024-07-20 14:08:32 -0400

    fix routing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     useEffect(() => {
         const fetchWorkouts = async () => {
             //get rid of /api/workouts, replace with address of web service
-            const response = await fetch('https://workoutlog-4z21.onrender.com/api/workouts');
+            const response = await fetch('https://workoutlog-4z21.onrender.com/');
             //turn the response into a json object
             console.log(response);
             const json = await response.json();
